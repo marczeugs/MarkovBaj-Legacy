a74295d00c1c5714acca970675cc1b1690e5e17b
marczeugs <eiscreme.softeis@googlemail.com>
2021-09-03 11:46:15 +0200

    Removed all asterisks from input data

--- getCommentsPushShift.ts
+++ getCommentsPushShift.ts
@@ -19,7 +19,7 @@ type ResponseFormat = {
 		const nextComments = (await fetch(urlTemplate + lastTimestamp).then((response: any) => response.json()) as ResponseFormat).data
 			.filter(comment => !['markovbaj', '[deleted]'].includes(comment.author.toLowerCase()));
 
-		allComments = [...allComments, ...nextComments.map(comment => comment.body.replace(/\[(.*)\]\(.*\)/g, (_, text) => text).replace(/["()]/g, '').trim())];
+		allComments = [...allComments, ...nextComments.map(comment => comment.body.replace(/\[(.*)\]\(.*\)/g, (_, text) => text).replace(/["()*]/g, '').trim())];
 
 		console.log(`Fetched ${nextComments.length} comments before ${new Date(lastTimestamp * 1000)}, total comments: ${allComments.length}`);
 		fs.writeFileSync('data.txt', JSON.stringify(allComments));
