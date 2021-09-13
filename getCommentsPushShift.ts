ae7da69e46d06846c48e17316fa138b1e3d5058e
marczeugs <eiscreme.softeis@googlemail.com>
2021-09-13 20:15:24 +0200

    Updated training data and added user blacklist

--- getCommentsPushShift.ts
+++ getCommentsPushShift.ts
@@ -10,6 +10,8 @@ type ResponseFormat = {
 }
 
 (async () => {
+	const blacklistedUsernames = fs.readFileSync('usernameBlacklist.txt', 'utf8').trim().split('\n');
+
 	const urlTemplate = 'https://api.pushshift.io/reddit/search/comment/?subreddit=forsen&size=500&before=';
 
 	let lastTimestamp = Math.floor(Date.now() / 1000);
@@ -19,9 +21,14 @@ type ResponseFormat = {
 		try {
 			const nextComments = (await fetch(urlTemplate + lastTimestamp).then((response: any) => response.json()) as ResponseFormat).data
 				.filter(comment => !['markovbaj', '[deleted]'].includes(comment.author.toLowerCase()));
-
-			allComments = [...allComments, ...nextComments.map(comment => comment.body.replace(/\[(.*)\]\(.*\)/g, (_, text) => text).replace(/["()*]/g, '').trim())];
-	
+			
+			allComments = [
+				...allComments, 
+				...nextComments
+					.filter(comment => !blacklistedUsernames.includes(comment.author))
+					.map(comment => comment.body.replace(/\[(.*)\]\(.*\)/g, (_, text) => text).replace(/["()*]/g, '').trim())
+			];
+			
 			console.log(`Fetched ${nextComments.length} comments before ${new Date(lastTimestamp * 1000)}, total comments: ${allComments.length}`);
 			fs.writeFileSync('data.txt', JSON.stringify(allComments));
 
@@ -30,6 +37,6 @@ type ResponseFormat = {
 			console.log(`Unable to fetch comments before ${new Date(lastTimestamp * 1000)}, retrying...`);
 		}
 
-		await new Promise(resolve => global.setTimeout(resolve, 5000));
+		await new Promise(resolve => global.setTimeout(resolve, 2000));
 	}
 })();
